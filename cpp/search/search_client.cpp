c6b018a40e420bd245993f8e3b880dd2a881db2c
jerryhanhuan <dengfeng2012@163.com>
2019-07-10 14:44:29 +0800

    add example search

---
+++ cpp/search/search_client.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+
+#include "search.grpc.pb.h"
+
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
+
+class SearchClient{
+
+    public:
+        SearchClient(std::shared_ptr<Channel> channel
+        : stub_(SearchService::NewStub(channel))){};
+    
+        std::string Search(const std::string& user){
+            SearchRequest request;
+            SearchResponse response;
+            request.set_request(user);
+
+            // clietn context 用来向服务器传递额外的信息，并调整某些 RPC 行为
+            ClientContext context;
+
+            //实际 RPC 调用
+            Status status = stub_->SearchService(&context,request,&response);
+            if(status.ok())
+            {
+                return response.response();
+            }else{
+                std::cout << status.error_code()<<": "<< status.error_message()<<std::endl;
+            }
+            return "RPC failed"
+
+        }
+
+
+};
+
+
+int main(int argc,char **argv)
+{
+
+    SearchClient cli(grpc::CreateChannel("localhost:50051",grpc::InsecureChannelCredentials()));
+    std::string user("yudf");
+    std::string reply = cli.Search(user);
+    std::cout <<"receive search result:"<<reply<<std::endl;
+    return 0;
+}
---
+++ cpp/search/search_server.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+
+#include "search.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+// final ,C++11标准中，引入的关键字，实现 final 类。一个类不能被别的类继承，这种类被称为 final 类
+// override 明确地表示一个函数是对基类中一个虚函数的重载
+class SearchRequestImpl final : public SearchService::Service
+{
+    Status SearchService(ServerContext *context, SearchRequest *request, SearchResponse *response) override
+    {
+        std::string prefix("Hello ");
+        response->set_response(prefix + request->request());
+        return Status::OK;
+    }
+};
+
+void RunServer()
+{
+    std::string server_address("0.0.0.0:50051");
+    SearchRequestImpl service;
+
+    ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    server->Wait();
+}
+
+int main(int argc,char **argv)
+{
+    RunServer();
+    return 0;
+}
