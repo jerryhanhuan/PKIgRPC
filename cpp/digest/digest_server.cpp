e2a55ae811e868e26e0d2ae2a1ffe4cfc28192c8
jerryhanhuan <dengfeng2012@163.com>
2019-07-25 17:11:22 +0800

    add digest test

--- cpp/digest/digest_server.cpp
+++ cpp/digest/digest_server.cpp
@@ -20,7 +20,7 @@ class DigestServiceImpl final : public DigestService::Service
     Status Digest(ServerContext *context, ServerReader<::DigestRequest> *reader, ::DigestResponse *response) override
     {
         DigestRequest request;
-        char digest_alg[32] = 0;
+        char digest_alg[32] = {0};
         unsigned char data[8192] = {0};
         int count = 0;
         while (reader->Read(&request))
@@ -38,26 +38,26 @@ class DigestServiceImpl final : public DigestService::Service
         unsigned char digest[32] = {0x12, 0x34, 0x00, 0x56, 0x78, 0xAB, 0xCD, 0xEF, 0xEF, 0xCD, 0xAB, 0x78, 0x56, 0x00, 0x34, 0x12};
         response->set_digest(digest);
         return Status::OK;
-    };
-
-    void RunServer()
-    {
-        std::string server_address("0.0.0.0:50051");
-        DigestServiceImpl service;
-
-        ServerBuilder builder;
-        builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-        builder.RegisterService(&service);
-
-        std::unique_ptr<Server> server(builder.BuildAndStart());
-        std::cout << "Server listening on " << server_address << std::endl;
-
-        server->Wait();
     }
+};
 
-    int main(int argc, char **argv)
-    {
-        RunServer();
-        return 0;
-    }
+void RunServer()
+{
+    std::string server_address("0.0.0.0:50051");
+    DigestServiceImpl service;
+
+    ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    server->Wait();
+}
+
+int main(int argc, char **argv)
+{
+    RunServer();
+    return 0;
 }
