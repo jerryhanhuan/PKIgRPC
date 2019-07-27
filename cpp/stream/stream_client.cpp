c348e404e3bfd462c04838a40c062871b0e4b835
jerryhanhuan <dengfeng2012@163.com>
2019-07-27 22:01:42 +0800

    add stream test

--- cpp/stream/stream_client.cpp
+++ cpp/stream/stream_client.cpp
@@ -31,9 +31,11 @@ public:
         StreamResData response;
 
         requset.set_req_data("client get server stream");
+
+        // 一个服务端流式RPC，客户端发送请求到服务器，拿到一个流去读取返回的消息序列
         std::unique_ptr<ClientReader<StreamResData>> cli_reader(
             stub_->GetStream(&context, requset));
-
+        // 客户端读取返回的流，直到里面没有任何消息
         while (cli_reader->Read(&response))
         {
             std::cout << "res::" << response.res_data() << std::endl;
@@ -55,12 +57,14 @@ public:
         ClientContext context;
         StreamReqData requset;
         StreamResData response;
+        // 一个服务端流式RPC，客户端发送请求到服务器， 获取一个写入流
         std::unique_ptr<ClientWriter<StreamReqData>> cli_writer(
             stub_->PutStream(&context, &response));
         char msg[3][256] = {"this is the first msg", "this is the second msg", "this is the third msg"};
         int i = 0;
         for (i = 0; i < 3; i++)
         {
+            // 客户端写入流不断地写入数据
             requset.set_req_data(msg[i]);
             if (!cli_writer->Write(requset))
             {
@@ -72,7 +76,7 @@ public:
         cli_writer->WritesDone();
         //Finish() 完成调用同时拿到 RPC 的状态
         Status status = cli_writer->Finish();
-         if (status.ok())
+        if (status.ok())
         {
             std::string result = response.res_data();
             std::cout << "res::" << result << std::endl;
@@ -86,8 +90,45 @@ public:
     }
 
     // 客户端-服务端 双流式
+    // 一个双向流式 RPC 是双方使用 读写流 去发送一个消息序列
+    // 两个流独立操作，client 和 server 可以以任意顺序读写
+    // 比如，服务器可以在写入响应前等待接收所有的客户端消息。或者可以交替的读取和写入消息，
+    // 或者其它读写顺序，每个流中的消息顺序被保留
     int allStream()
     {
+        ClientContext context;
+        std::shared_ptr<ClientReaderWriter<StreamReqData, StreamResData> > stream(
+        stub_->AllStream(&context));
+
+        //先 write 数据到服务端
+        char msg[3][256] = {"this is the first msg", "this is the second msg", "this is the third msg"};
+        int i = 0;
+        StreamReqData requset;
+        StreamResData response;
+        for (i = 0; i < 3; i++)
+        {
+            // 客户端写入流不断地写入数据
+            requset.set_req_data(msg[i]);
+            if (!stream->Write(requset))
+            {
+                // Broken stream.
+                break;
+            }
+        }
+        //WritesDone() 通知gRPC 我们已经完成输入
+        stream->WritesDone();
+        //read 
+        while (stream->Read(&response))
+        {
+            std::cout << "res::" << response.res_data() << std::endl;
+        }
+        //Finish() 完成调用同时拿到 RPC 的状态
+        Status status = stream->Finish();
+        if (!status.ok())
+        {
+            std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+            return -1;
+        }
         return 0;
     }
 
@@ -111,5 +152,13 @@ int main(int argc, char **argv)
     {
         std::cout << "putStream failed !"<< std::endl;
     }
+
+    std::cout <<"  ========================= double stream ========================= "<< std::endl;
+
+    if(cli.allStream()<0)
+    {
+        std::cout << "allStream failed !"<< std::endl;
+    }
+    
     return 0;
 }
--- cpp/stream/stream_server.cpp
+++ cpp/stream/stream_server.cpp
@@ -5,66 +5,76 @@
 #include <grpcpp/grpcpp.h>
 #include "stream.grpc.pb.h"
 
-
-
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::ServerReader;
+using grpc::ServerReaderWriter;
 using grpc::ServerWriter;
 using grpc::Status;
-using grpc::ServerReaderWriter;
 
 using std::cout;
 using std::endl;
 
-
-class streamServiceImpl final: public Greeter::Service{
+class streamServiceImpl final : public Greeter::Service
+{
 
     // 服务端流式
     // 服务端给客户端返回一个流式数据
-    Status GetStream(ServerContext* context, const StreamReqData* request, ServerWriter<StreamResData>* writer) override
+    Status GetStream(ServerContext *context, const StreamReqData *request, ServerWriter<StreamResData> *writer) override
     {
-            std::string req = request -> req_data();
-            cout << "req::" << req <<endl;
-            int i = 0;
-            StreamResData response;
-            char msg[3][256]={"1111","2222","3333"};
-            for(i = 0; i< 3;i++)
-            {
-                response.set_res_data(msg[i]);
-                writer->Write(response);
-            }
-            return Status::OK;
-    }
-
-
-    // 客户端流式
-    // 服务端接收客户端一个流式数据
-    Status PutStream(ServerContext* context,ServerReader< StreamReqData>* reader, StreamResData* response) override
-    {
-            StreamReqData request;
-            while(reader->Read(&request))
-            {
-                std::string req = request.req_data();
-                cout << "req::"<<req <<endl;
-            }
-            response->set_res_data("recv ok");
-            return Status::OK;
-    }
-
-
-    //双向流
-    Status AllStream(ServerContext* context, ServerReaderWriter< StreamResData, StreamReqData>* stream) override
-    {
-
-           
-
-
+        std::string req = request->req_data();
+        cout << "req::" << req << endl;
+        int i = 0;
+        StreamResData response;
+        char msg[3][256] = {"1111", "2222", "3333"};
+        for (i = 0; i < 3; i++)
+        {
+            response.set_res_data(msg[i]);
+            writer->Write(response);
+        }
         return Status::OK;
     }
 
+    // 客户端流式
+    // 服务端接收客户端一个流式数据
+    Status PutStream(ServerContext *context, ServerReader<StreamReqData> *reader, StreamResData *response) override
+    {
+        StreamReqData request;
+        while (reader->Read(&request))
+        {
+            std::string req = request.req_data();
+            cout << "req::" << req << endl;
+        }
+        response->set_res_data("recv ok");
+        return Status::OK;
+    }
 
+    //双向流
+    // 一个双向流式 RPC 是双方使用 读写流 去发送一个消息序列
+    // 两个流独立操作，client 和 server 可以以任意顺序读写
+    // 比如，服务器可以在写入响应前等待接收所有的客户端消息。或者可以交替的读取和写入消息，
+    // 或者其它读写顺序，每个流中的消息顺序被保留
+    Status AllStream(ServerContext *context, ServerReaderWriter<StreamResData, StreamReqData> *stream) override
+    {
+
+        // 这里的示例是 先读取所有客户端输入，再返回流到客户端
+        StreamReqData request;
+        while (stream->Read(&request))
+        {
+            std::string req = request.req_data();
+            cout << "req::" << req << endl;
+        }
+
+        StreamResData response;
+        char msg[3][256] = {"1111", "2222", "3333"};
+        for (i = 0; i < 3; i++)
+        {
+            response.set_res_data(msg[i]);
+            writer->Write(response);
+        }
+        return Status::OK;
+    }
 };
 
 void RunServer()
@@ -87,7 +97,3 @@ int main(int argc, char **argv)
     RunServer();
     return 0;
 }
-
-
-
-
