236eb9dbc663c3346267b5255fd44d5095d5f2cb
jerryhanhuan <dengfeng2012@163.com>
2019-07-29 20:57:47 +0800

    add msgsearch

--- cpp/msgsearch/msgsearch_client.cpp
+++ cpp/msgsearch/msgsearch_client.cpp
@@ -11,9 +11,68 @@ using grpc::Channel;
 using grpc::ClientContext;
 using grpc::Status;
 
+class msgSearchClient
+{
+
+public:
+    msgSearchClient(std::shared_ptr<Channel> channel)
+        : stub_(MsgService::NewStub(channel)) {}
+
+    int msgSearch(std::string name, std::string id)
+    {
+        MsgRequest request;
+        MsgResponse response;
+        char address[256] = {0};
+        char phonenum[32] = {0};
+        if (name.size())
+        {
+            request.set_name(name);
+        }
+        else
+        {
+            request.set_id(id);
+        }
+        // clietn context 用来向服务器传递额外的信息，并调整某些 RPC 行为
+        ClientContext context;
+        //实际 RPC 调用
+        Status status = stub_->Search(context, &request, &response);
+        if (status.ok())
+        {
+            MsgResponse::RsInfoCase t_case = response->rs_info_case();
+            switch (t_case)
+            {
+            case MsgResponse::kAddress: //has_address
+                memcpy(address, response->address().data(), response->address().size());
+                printf("address::%s .\n", address);
+                break;
+            case MsgResponse::kPhonenumber: //has_phonenumber
+                memcpy(phonenum, response->phonenumber().data(), response->phonenumber().size());
+                printf("phonenum::%s .\n", phonenum);
+                break;
+            case MsgResponse::RS_INFO_NOT_SET: //has not name or id
+                printf("not has any address or phonenumber .\n");
+                break;
+            }
+        }
+        else
+        {
+            std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+            return -1;
+        }
+    }
+
+private:
+    // 注意，这个不要忘
+    std::unique_ptr<msgSearchClient::Stub> stub_;
+};
 
 int main()
 {
+    std::string name = "yexb";
+    std::string id = "1234567";
+
+    msgSearch(name,"");
+    msgSearch("",id);
 
     return 0;
 }
--- cpp/msgsearch/msgsearch_server.cpp
+++ cpp/msgsearch/msgsearch_server.cpp
@@ -6,20 +6,39 @@
 
 #include "msg.grpc.pb.h"
 
-
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::Status;
 
-class MsgServiceImpl final:public MsgService::Service{
+class MsgServiceImpl final : public MsgService::Service
+{
 
-    Status Search(ServerContext* context, MsgRequest* request, MsgResponse* response)
+    Status Search(ServerContext *context, const MsgRequest *request, MsgResponse *response)
     {
         MsgRequest::RqInfoCase t_case = request->rq_info_case();
+        char name[256] = {0};
+        char id[20] = {0};
+        std::string address = "wuhan";
+        std::string phonenumber = "18507148681";
+        switch (t_case)
+        {
+        case MsgRequest::kName: //has_name
+            memcpy(name, request->name().data(), request->name().size());
+            printf("name::%s .\n", name);
+            response -> set_address(address);
+            break;
+        case MsgRequest::kId: //has_id
+            memcpy(id, request->id().data(), request->name().size());
+            printf("id::%s .\n", id);
+            response -> set_phonenumber(phonenumber);
+            break;
+        case MsgRequest::RS_INFO_NOT_SET: //has not name or id
+            printf("not has any name or id .\n");
+            break;
+        }
         return Status::OK;
     }
-
 };
 
 void RunServer()
@@ -42,5 +61,3 @@ int main(int argc, char **argv)
     RunServer();
     return 0;
 }
-
-
