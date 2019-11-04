349d495cde9e7c324405eec3eebcea59b1244c3f
jerryhanhuan <dengfeng2012@163.com>
2019-11-04 11:31:36 +0800

    add transferfile server and client

---
+++ cpp/transferfile/file_client.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+
+int main(int argc, char **argv)
+{
+    string filename;
+    cout << "请输入文件名::" << endl;
+    cin >> filename;
+
+    return 0;
+
+}
---
+++ cpp/transferfile/file_server.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <fstream>
+
+
+
+#include <grpcpp/grpcpp.h>
+#include "transferfile.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::ServerReader;
+using grpc::ServerReaderWriter;
+using grpc::ServerWriter;
+using grpc::Status;
+
+
+using std::cout;
+using std::endl;
+
+#define CHUNK_SIZE 1024 * 1024
+
+class TransferFileImpl  final:public TransferFile::Service{
+    Status Upload(ServerContext* context, ServerReader<Chunk>* reader,Reply* response);
+};
+
+
+
+
+Status TransferFileImpl::Upload(ServerContext* context, ServerReader<Chunk>* reader,Reply* response)
+{
+
+    return Status::OK;
+}
+
+
+void RunServer()
+{
+    std::string server_address("0.0.0.0:50051");
+    TransferFileImpl service;
+
+    ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    server->Wait();
+}
+
+int main(int argc, char **argv)
+{
+    RunServer();
+    return 0;
+}
+
+
