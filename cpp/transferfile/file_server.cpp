ace245d9e3282e31bca761365fa52ca457914115
jerryhanhuan <dengfeng2012@163.com>
2019-11-04 13:51:23 +0800

    add file_server.cpp

--- cpp/transferfile/file_server.cpp
+++ cpp/transferfile/file_server.cpp
@@ -32,6 +32,24 @@ class TransferFileImpl  final:public TransferFile::Service{
 Status TransferFileImpl::Upload(ServerContext* context, ServerReader<Chunk>* reader,Reply* response)
 {
 
+    Chunk chunk;
+    const char *des_file_name = "server_tmp";
+    std::ofstream outfile;
+    std::string data;
+
+    // out 文件以输出方式打开(写文件)
+    // trunc 如果文件存在，把文件长度设置为0
+    // binary 以二进制方式读写文件
+    outfile.open(des_file_name,std::ofstream::out | std::ofstream::trunc | std::ofstream::binary);
+    while(reader->Read(&chunk)){
+        data = chunk.buffer();
+        outfile.write(data,data.length());
+    }
+    //tellp 用于返回写入位置
+    //tellg 则用于返回读取位置
+    long pos = outfile.tellp();
+    outfile.close();
+    Reply->set_length(pos); 
     return Status::OK;
 }
 
