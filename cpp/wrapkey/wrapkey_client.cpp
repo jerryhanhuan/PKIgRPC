a1e679bc5cd22ce038eeb9e09a919ce6068b0c6f
jerryhanhuan <dengfeng2012@163.com>
2019-11-05 22:49:10 +0800

    add demo

---
+++ cpp/wrapkey/wrapkey_client.cpp
@@ -0,0 +1,84 @@
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+#include <grpcpp/grpcpp.h>
+#include "wrapkeys.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+class WrapkeyClient
+{
+
+public:
+    WrapkeyClient(std::shared_ptr<Channel> channel)
+        : stub_(WrapKey::NewStub(channel)) {}
+
+    int Wrapkey()
+    {
+        int ret = 0;
+        Cipher request;
+        Cipher response;
+        // clietn context 用来向服务器传递额外的信息，并调整某些 RPC 行为
+        ClientContext context;
+        string transformation = "DES/ECB";
+        request.set_transformation(transformation);
+
+        vector<string> messageV;
+        messageV.push_back("11111111");
+        messageV.push_back("22222222");
+        messageV.push_back("33333333");
+
+        for (auto msg : messageV)
+        {
+            request.add_messages(msg);
+        }
+
+        vector<string> keyV;
+        keyV.push_back("0123456789ABCDEFFEDCBA9876543210");
+        keyV.push_back("00000000000000000000000000000000");
+        keyV.push_back("FEDCBA98765432100123456789ABCDEF");
+
+        for (auto key : keyV)
+        {
+            Key *t = request.add_wrappingkeys();
+            t->set_secret_key(key);
+        }
+        //实际 RPC 调用
+        Status status = stub_->Wrap(&context, request, &response);
+        if (status.ok())
+        {
+            cout << "Wrap Key ok" << endl;
+        }
+        else
+        {
+            std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+            return -1;
+        }
+        return 0;
+    }
+
+private:
+    // 注意，这个不要忘
+    std::unique_ptr<WrapKey::Stub> stub_;
+};
+
+
+
+int main(int argc, char **argv)
+{
+
+    WrapkeyClient cli(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
+    cli.Wrapkey();
+    return 0;
+}
---
+++ cpp/wrapkey/wrapkey_server.cpp
@@ -0,0 +1,70 @@
+
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+#include <grpcpp/grpcpp.h>
+#include "wrapkeys.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+class WrapkeyImpl final : public WrapKey::Service
+{
+
+public:
+    Status Wrap(ServerContext *context, const Cipher *request, Cipher *response) override
+    {
+        string data;
+        cout << "transformation::" << request->transformation << endl;
+        for(auto &msg:request->messages)
+        {
+            data += msg;
+        }
+        cout << "msg:" << endl;
+        for(auto key:request->wrappingkeys())
+        {
+            cout << "key::" << key.secret_key() << endl;
+        }
+        return Status::OK;
+    }
+};
+
+
+
+void RunServer()
+{
+    std::string server_address("0.0.0.0:50051");
+    WrapkeyImpl service;
+
+    ServerBuilder builder;
+    //builder.SetOption(std::unique_ptr<grpc::ServerBuilderOption>(new NoReusePortOption));
+     // Listen on the given address without any authentication mechanism.
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+
+    // Register "service" as the instance through which we'll communicate with
+    // clients. In this case it corresponds to an *synchronous* service.
+    builder.RegisterService(&service);
+
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    // Wait for the server to shutdown. Note that some other thread must be
+    // responsible for shutting down the server for this call to ever return.
+    server->Wait();
+}
+
+int main(int argc,char **argv)
+{
+    RunServer();
+    return 0;
+}
